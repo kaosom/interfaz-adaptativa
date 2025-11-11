a3b543adc844b3d3611dbf3725cc24387043ce4c
Angel <angelsoatwork@gmail.com>
2025-11-11 13:02:55 -0600

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import InterfazEstresante from './components/InterfazEstresante';
+import InterfazAdaptada from './components/InterfazAdaptada';
+import { useDeteccionEstres } from './hooks/useDeteccionEstres';
 
 function App() {
+  const [esEstresado, setEsEstresado] = useState(false);
+
+  useDeteccionEstres(setEsEstresado);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {esEstresado ? <InterfazAdaptada /> : <InterfazEstresante />}
     </div>
   );
 }
---
+++ src/components/InterfazAdaptada.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './InterfazAdaptada.css';
+
+const InterfazAdaptada = () => {
+  return (
+    <div className="interfaz-adaptada">
+      <div style={{ padding: '40px', textAlign: 'center' }}>
+        <h1>Interfaz Adaptada</h1>
+        <p>Placeholder - Diego completará este componente</p>
+      </div>
+    </div>
+  );
+};
+
+export default InterfazAdaptada;
+
